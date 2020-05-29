517d5901cae8264eb9fdc247dc8899afceb8232f
Aleksandr Trebushchuk <zamsion@yandex.ru>
2020-05-30 01:02:04 +0700

    Clang Format fix

--- test/search_array_testing.c
+++ test/search_array_testing.c
@@ -1,6 +1,4 @@
 #include <ctest.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <search_array.h>
 #include <string.h>
 CTEST(working_with_array, is_valid_string)
@@ -34,25 +32,25 @@ CTEST(working_with_array, is_valid_string)
 
 CTEST(working_with_array, split_by_space)
 {
-    //Given
+    // Given
     int i;
     char* str = (char*)malloc(15 * sizeof(char));
     strcpy(str, "How this works\0");
     int* words = (int*)malloc(3 * sizeof(int));
-    //When
+    // When
     split_by_space(str, words);
-    //Then
-    int expected_1[3] = {0,4,9};
+    // Then
+    int expected_1[3] = {0, 4, 9};
 
     char* expected_2 = (char*)malloc(15 * sizeof(char));
     strcpy(expected_2, "How this works\0");
     for (i = 0; i < 15; i++) {
-    if (expected_2[i] == ' ')
-    expected_2[i] = 0;
+        if (expected_2[i] == ' ')
+            expected_2[i] = 0;
     }
-    //Comprassion
+    // Comprassion
     for(i = 0; i < 3; i++)
-    ASSERT_EQUAL(expected_1[i], words[i]);
+        ASSERT_EQUAL(expected_1[i], words[i]);
 
     ASSERT_STR(expected_2, str);
 }
